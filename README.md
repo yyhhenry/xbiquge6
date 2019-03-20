8700114b9d72e734d1b46f24959a78f851a3b38b
yyhhenry <yyhhenry@foxmail.com>
2019-03-20 12:09:13 +0800

    school

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 用于下载xbiquge6.com的小说
 
-[下载脚本](https://github.com/yyhhenry/xbiquge6/raw/master/main.js)
+[下载脚本](https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6/main.js)
--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 // @description  下载小说
 // @author       yyhhenry
 // @match        https://www.xbiquge6.com/*
-// @updateURL    https://github.com/yyhhenry/xbiquge6/raw/master/main.js
-// @downloadURL  https://github.com/yyhhenry/xbiquge6/raw/master/main.js
+// @updateURL    https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6/main.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6/main.js
 // @supportURL   https://github.com/yyhhenry/xbiquge6
 // ==/UserScript==
 'use strict';
