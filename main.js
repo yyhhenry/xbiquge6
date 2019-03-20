daaed3cd3fe4b767eacf18fb324e22245ba7f502
yyhhenry <yyhhenry@foxmail.com>
2019-03-20 15:22:29 +0800

    school

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 // @description  下载小说
 // @author       yyhhenry
 // @match        https://www.xbiquge6.com/*
-// @updateURL    https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6@v1.1/main.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6@v1.1/main.js
+// @updateURL    https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6@latest/main.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/yyhhenry/xbiquge6@latest/main.js
 // @supportURL   https://github.com/yyhhenry/xbiquge6
 // ==/UserScript==
 
